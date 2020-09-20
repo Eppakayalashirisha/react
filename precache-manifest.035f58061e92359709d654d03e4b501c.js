f1b0af44a6462fd55d37dabe90b309d277757272
Eppakayala shirisha <shirisha.eppakayala8@gmail.com>
2020-09-20 08:56:01 +0200

    Updates

---
+++ precache-manifest.035f58061e92359709d654d03e4b501c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e6008676eb65d6267cf8a2b77acfe6",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "1bb901b861e6124d73a2",
+    "url": "/react/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "fee33e56ae5be17ffe4e",
+    "url": "/react/static/css/main.686842f3.chunk.css"
+  },
+  {
+    "revision": "1bb901b861e6124d73a2",
+    "url": "/react/static/js/2.384f0e9c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react/static/js/2.384f0e9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee33e56ae5be17ffe4e",
+    "url": "/react/static/js/main.5621167a.chunk.js"
+  },
+  {
+    "revision": "7c519d5300abae978a5b",
+    "url": "/react/static/js/runtime-main.7b3857cb.js"
+  }
+]);
